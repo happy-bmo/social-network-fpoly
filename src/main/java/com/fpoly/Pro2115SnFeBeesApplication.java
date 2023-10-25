5e67a61c4bb9c0a3e77e5030fda3a008b40971a0
dyck <zeus090803@gmail.com>
2023-10-25 10:08:47 +0700

    First commit

---
+++ src/main/java/com/fpoly/Pro2115SnFeBeesApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro2115SnFeBeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro2115SnFeBeesApplication.class, args);
+	}
+
+}
