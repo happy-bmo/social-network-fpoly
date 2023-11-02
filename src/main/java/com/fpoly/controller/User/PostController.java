56c516d57b87d6af1095a4b8e686900a75ab2b1e
dyck <143906726+happy-bmo@users.noreply.github.com>
2023-11-02 11:12:05 +0700

    Storage template user

--- src/main/java/com/fpoly/controller/User/PostController.java
+++ src/main/java/com/fpoly/controller/User/PostController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-    @RequestMapping("/")
-    public String index() {
-        return "redirect:/newfeed";
+public class PostController {
+    @RequestMapping("/newfeed")
+    public String newfeed() {
+        return "views/user/post/newfeed";
     }
 }
